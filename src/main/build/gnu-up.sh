ef5b0a0b8ec21cbaf2d72f40fb9de9bd9acec30a
Willem Cazander <w.cazander@gmail.com>
2013-04-29 01:43:48 +0200

    Updated license to 2013.

--- src/main/build/gnu-up.sh
+++ src/main/build/gnu-up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2013, Willem Cazander
+# Copyright (c) 2004-2013, Willem Cazander
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided
@@ -20,9 +20,6 @@
 # HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-#
-# Small script to sign and upload files to savannah
 #
 
 if [ "" == "$1" ]; then
--- x4o-driver/src/test/java/org/x4o/xml/test/swixml/conv/BorderConverter.java
+++ x4o-driver/src/test/java/org/x4o/xml/test/swixml/conv/BorderConverter.java
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2004-2012, Willem Cazander
+ * Copyright (c) 2004-2013, Willem Cazander
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided
  * that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this list of conditions and the
  *   following disclaimer.
  * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
  *   the following disclaimer in the documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -20,7 +20,6 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package	org.x4o.xml.test.swixml.conv;
 
 import java.util.Locale;
